cce217e4fbc1319a1993ebf461f93c1d3d282234
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-07-23 11:58:52 +0100

    Sesond interface

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <script setup>
-import Chatboot from '../components/Chatboot.vue'
+import Chatboot1 from '../components/Chatboot1.vue'
+import Chatboot2 from '../components/Chatboot2.vue'
 </script>
 
 <template>
-  <main>
-    <Chatboot />
+  <main class="container">
+    <Chatboot1 />
+    <Chatboot2 />
   </main>
 </template>
+
+<style>
+.container {
+  border: 1px solid red;
+  display: flex;
+}
+</style>
