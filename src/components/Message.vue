6b562f727adc48fc18be278617b4c70c7a34ef72
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-07-26 09:58:46 +0100

    font size

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -10,8 +10,8 @@ export default {
   <div class="d-flex justify-content-between mb-1" style="width: 95%; margin: 0px auto;">
     <img src="../assets/home6.jpg" class="rounded-circle mx-2" style="width: 40px; height: 40px" />
     <div class="mt-2">
-      <h5 style="font-weight: bold" class="fs-7">{{ name }}</h5>
-      <p class="fs-7">{{ message }}</p>
+      <h5 style="font-weight: bold;font-size: 15px;">{{ name }}</h5>
+      <p style="font-size: 13px;">{{ message }}</p>
     </div>
   </div>
 </template>
