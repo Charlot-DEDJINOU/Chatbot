ce502d3b228e97650609e145992e0b0a1e52740c
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-02-19 19:30:24 +0100

    npm run format

--- frontend/src/components/Message.vue
+++ frontend/src/components/Message.vue
@@ -7,11 +7,14 @@ export default {
 }
 </script>
 <template>
-  <div class="d-flex justify-content-between mb-2 border-bottom" style="width: 95%; margin: 0px auto">
+  <div
+    class="d-flex justify-content-between mb-2 border-bottom"
+    style="width: 95%; margin: 0px auto"
+  >
     <img src="../assets/home6.jpg" class="rounded-circle mx-2" style="width: 40px; height: 40px" />
     <div class="mt-2" style="width: 85%">
       <h5 style="font-weight: bold; font-size: 15px">{{ name }}</h5>
-      <span style="font-size: 13px; padding-bottom: 2px;">{{ message }}</span>
+      <span style="font-size: 13px; padding-bottom: 2px">{{ message }}</span>
     </div>
   </div>
 </template>
