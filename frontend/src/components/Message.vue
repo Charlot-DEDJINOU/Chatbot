c248895c89bbb7c7e0de066010beb3871a7b1564
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-02-23 14:12:01 +0100

    remove font family

--- frontend/src/components/Message.vue
+++ frontend/src/components/Message.vue
@@ -11,7 +11,8 @@ export default {
     class="d-flex justify-content-between mb-2 border-bottom"
     style="width: 95%; margin: 0px auto"
   >
-    <img src="../assets/home6.jpg" class="rounded-circle mx-2" style="width: 40px; height: 40px" />
+    <img v-if="name !== 'Charlot Joël'" src="../assets/home6.jpg" class="rounded-circle mx-2" style="width: 40px; height: 40px" />
+    <img v-else src="../assets/charlot.jpg" class="rounded-circle mx-2" style="width: 40px; height: 40px" />
     <div class="mt-2" style="width: 85%">
       <h5 style="font-weight: bold; font-size: 15px">{{ name }}</h5>
       <span style="font-size: 13px; padding-bottom: 2px">{{ message }}</span>
